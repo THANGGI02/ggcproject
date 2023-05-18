bfecd8c20b96930712a0c0c9786841bbebb22a1c
THANG NGUYEN <105684222+THANGGI02@users.noreply.github.com>
2023-05-18 17:10:21 +0700

    FORM QUÊN MẬT KHẨU
    Tui thêm màu cho nút text và padding-left
    Tui thay đổi button home thành thẻ a cho dễ css

--- forget_password.php
+++ forget_password.php
@@ -10,46 +10,48 @@
     <title>Quên mật khẩu</title>
     <link rel="stylesheet" href="Basic-form.css">
     <style>
-    .container {
-        width: 450px;
-        height: 300px;
-    }
+           .container {
+               width: 450px;
+               height: 300px;
+            }
 
-    table {
-        height: 120px;
-        padding-top: 10px;
-        transform: translateX(30px);
-    }
+            table {
+                height: 120px;
+                padding-top: 10px;
+                transform: translateX(30px);
+            }
 
-    /*CSS cho các cột trong table */
-    table tr td {
-        padding-top: 10px;
-    }
+           /*CSS cho các cột trong table */
+           table tr td {
+                padding-top: 10px;
+            }
 
-    /*CSS cho các label Username và Password*/
-    label {
-        font-size: 20px;
-    }
+           /*CSS cho các label Username và Password*/
+           label {
+                font-size: 20px;
+           }
 
-    /*CSS cho dữ liệu đầu vào dạng text và password*/
-    input[type='text']{
-        width: 200px;
-        height: 25px;
-        background-color: #a39393;
-        border: none;
-        margin-left: 10px;
-    }
+           /*CSS cho dữ liệu đầu vào dạng text và password*/
+           input[type='text']{
+                width: 200px;
+                height: 25px;
+                background-color: whitesmoke;
+                color: rgba(0, 0, 0, 0.733); 
+                border: none;
+                padding-left: 5px;
+                margin-left: 10px;
+            }
     </style>
 </head>
 
 <body>
     <div class="container">
     <div>
-        <button type="button" id="btn_homepage" onclick="location.href='./index.php'">
-            <svg xmlns="http://www.w3.org/2000/svg" width="64pt" height="64pt" viewBox="0 0 64 64" style="isolation:isolate" id="home"><defs><clipPath id="a"><rect width="64" height="64"></rect></clipPath></defs><g clip-path="url(#a)"><path d=" M 43.045 61.369 L 59.016 61.369 C 60.669 61.369 62.006 60.027 62 58.374 L 61.894 29.675 C 61.89 28.573 61.214 27.088 60.387 26.361 L 34.314 3.452 C 33.072 2.361 31.053 2.357 29.806 3.443 L 3.498 26.367 C 2.667 27.091 1.996 28.573 2 29.675 L 2.106 58.374 C 2.112 60.027 3.459 61.369 5.112 61.369 L 21.084 61.369 C 22.737 61.369 24.078 60.027 24.078 58.374 L 24.078 45.397 C 24.078 44.295 24.973 43.401 26.075 43.401 L 38.054 43.401 C 39.155 43.401 40.05 44.295 40.05 45.397 L 40.05 58.374 C 40.05 60.027 41.392 61.369 43.045 61.369 Z "></path></g>
-            </svg>
-        </button>
-      </div>
+                <a href="./index.php">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="64pt" height="64pt" viewBox="0 0 64 64" style="isolation:isolate" id="home"><defs><clipPath id="a"><rect width="64" height="64"></rect></clipPath></defs><g clip-path="url(#a)"><path d=" M 43.045 61.369 L 59.016 61.369 C 60.669 61.369 62.006 60.027 62 58.374 L 61.894 29.675 C 61.89 28.573 61.214 27.088 60.387 26.361 L 34.314 3.452 C 33.072 2.361 31.053 2.357 29.806 3.443 L 3.498 26.367 C 2.667 27.091 1.996 28.573 2 29.675 L 2.106 58.374 C 2.112 60.027 3.459 61.369 5.112 61.369 L 21.084 61.369 C 22.737 61.369 24.078 60.027 24.078 58.374 L 24.078 45.397 C 24.078 44.295 24.973 43.401 26.075 43.401 L 38.054 43.401 C 39.155 43.401 40.05 44.295 40.05 45.397 L 40.05 58.374 C 40.05 60.027 41.392 61.369 43.045 61.369 Z "></path></g>
+                    </svg>
+                </a>
+    </div>
       <label for="" style="font-size: 28px;">Quên mật khẩu</label>
 
       <div id="submit-form" name="submit-form">
